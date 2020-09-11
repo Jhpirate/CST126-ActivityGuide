eadb6f053b5209871a1500572c5f3c9c1dbfbd4f
jaredheeringa <jaredheeringa@icloud.com>
2020-09-11 17:03:32 -0500

    CST135 Activity #1. HTML form and PHP script to echo the values entered. No database connection.

---
+++ pageHandler.php
@@ -0,0 +1,12 @@
+<?php 
+
+echo "First Name is " . $_POST["fName"]; //fName (First Name) field from register.html form
+echo '<br>';
+
+echo "Last Name is " . $_POST["lName"]; //lName (Last Name) field from register.html form
+
+echo '<br>';
+echo "You are now registered.";
+
+
+?>
