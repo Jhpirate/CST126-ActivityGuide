9b6d5d116ffbb470bef99a94018f26f6a9c0d8b0
jaredheeringa <jaredheeringa@icloud.com>
2020-09-14 16:33:12 -0500

    Activity #1 Done. Get data from html form (register.html) and stored in SQL databse.

--- pageHandler.php
+++ pageHandler.php
@@ -1,12 +1,30 @@
-<?php 
+<?php
+/*
+Author: Jared Heeringa
+Date: 09/13/2020
+Class: CST126
+Project: Activity #1
+*/
 
-echo "First Name is " . $_POST["fName"]; //fName (First Name) field from register.html form
-echo '<br>';
+// Original PHP code to output what the user entered
+//
+// echo "First Name is " . $_POST["fName"]; //fName (First Name) field from register.html form
+// echo '<br>';
+// echo "Last Name is " . $_POST["lName"]; //lName (Last Name) field from register.html form
+// echo '<br>';
+// echo "You are now registered.";
 
-echo "Last Name is " . $_POST["lName"]; //lName (Last Name) field from register.html form
+$firstName = $_POST["fName"]; //Uses the 'name' field from html form
+$lastName = $_POST["lName"];
 
-echo '<br>';
-echo "You are now registered.";
+$data_link = mysqli_connect("localhost", "root", "root", "activity1");
 
+//VERY BAD! SQL injection is possible in the current configuration. Don't know how to santize input right now, so this is how its going to be.
+$sql_statement = "INSERT INTO users (FIRST_NAME, LAST_NAME) VALUES ('$firstName', '$lastName')"; 
+mysqli_query($data_link, $sql_statement);
+
+mysqli_close();
+
+echo "<p><strong>Form Sucessfully Submitted.</strong></p>";
 
 ?>
