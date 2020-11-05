2d541735516bafdac48581f78c3be657664a8857
Jared Heeringa <jaredheeringa@icloud.com>
2020-11-04 17:16:15 -0700

    (Activity #5) Working with HTTP get in php

---
+++ activity5/makeRequest.php
@@ -0,0 +1,9 @@
+<?php
+
+// get values from the URL
+$id = $_GET["id"];
+$mode = $_GET["mode"];
+
+// echo the values out to the screen
+echo "ID: " . $id . "<br/>";
+echo "Mode: " . $mode . "<br/>";
